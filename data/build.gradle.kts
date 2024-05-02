6e384097db437b0576fe7a8a2d3176542917004b
Pegbeer <carlosgarciahl@hotmail.com>
2024-05-02 13:19:23 -0600

    Data module setup

--- data/build.gradle.kts
+++ data/build.gradle.kts
@@ -34,9 +34,5 @@ android {
 dependencies {
 
     implementation(libs.core.ktx)
-    implementation(libs.androidx.appcompat)
-    implementation(libs.material)
     testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.test.ext.junit)
-    androidTestImplementation(libs.espresso.core)
 }
